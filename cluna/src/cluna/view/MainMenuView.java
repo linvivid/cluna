aff90fcce0ca71891fe1c093d78499f53ab076bb
gardensun <gardensun@TheInfomaster>
2016-02-18 22:02:37 -0700

    Half of Team Assignment 6

---
+++ cluna/src/cluna/view/MainMenuView.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cluna.view;
+
+/**
+ *
+ * @author gardensun
+ */
+public class MainMenuView {
+    
+    private final String MENU = "\n"
+            +"\n-------------------------"
+            +"\n| Main Menu             |"
+            +"\n|nG - New Game"
+            +"\n|nH - Get Help"
+            +"\n|nS - Save Game"
+            +"\n|nE - Exit"
+            +"\n-------------------------";
+    
+    public void displayMenu(){
+        char selection = ' ';
+        
+        do{
+            System.out.println(MENU);
+            
+            String input = this.getInput();
+            selection = input.charAt(0);
+            
+            this.doAction(selection);
+                       
+        } while (selection != 'E');
+    }
+    
+    private String getInput(){
+        Scanner keyboard= new Scanner(System.in);
+        String input = " ";
+        boolean validInput = false;
+        
+        while (!validInput) {
+            input = keyboard.nextLine();
+            input = input.trim();
+        
+            if(input.length() < 1){
+                System.out.println("\nInvalid value: You must enter a character.");
+                continue;
+            }
+            break; 
+        }
+        return input;
+    }
+    
+    public void doAction(char selection){
+        
+        switch (selection) {
+            case 'G':
+                this.startNewGame();
+                break;
+            case 'H':
+                this.displayHelpMenu();
+                break;
+            case 'S':
+                this.saveGame();
+                break;
+            case 'E':
+                return;
+            default:
+                System.out.println("\nInvalid. Try again.");
+                break;
+        }
+    }
+}
+
